2c6e6888e2d3586fa223fdd4880cfca125880ac0
Paul Wilson <paulanthonywilson@gmail.com>
2013-11-23 15:21:57 +0000

    more undisciplined hacking

--- many_leds/many_leds.ino
+++ many_leds/many_leds.ino
@@ -5,28 +5,28 @@ void setup(){
   for(int i=13; i > 8; i--){
     pinMode(i, OUTPUT);
   }
-  
+
   pinMode(button, INPUT);
   digitalWrite(button, HIGH);
 }
 
 void loop(){
-  
+
   int buttonState = digitalRead(button);
   Serial.println(buttonState);
   if(0 == buttonState){
-   
-    
-  
-  for(int i=13; i > 8; i--){
-    digitalWrite(i, HIGH);
-    delay(100);
+
+
+
+    for(int i=13; i > 8; i--){
+      digitalWrite(i, HIGH);
+      delay(100);
+    }
+
+    for(int i=13; i > 8; i--){
+      digitalWrite(i, LOW);
+      delay(100);
+    }
   }
-  
-  for(int i=13; i > 8; i--){
-    digitalWrite(i, LOW);
-    delay(100);
-  }
-  }  
-  
+
 }
--- resistor/resistor.ino
+++ resistor/resistor.ino
@@ -1,1 +1,35 @@
+const int button = 8;
 
+void setup(){
+  Serial.begin(9600);
+  for(int i=13; i > 8; i--){
+    pinMode(i, OUTPUT);
+  }
+
+  pinMode(button, INPUT);
+  digitalWrite(button, HIGH);
+
+}
+
+void loop(){
+ int delayValue = analogRead(0);
+  Serial.print("Analog reading: ");
+  Serial.println(delayValue);
+  
+  int buttonState = digitalRead(button);
+  if(1 == buttonState){
+
+
+
+    for(int i=13; i > 8; i--){
+      digitalWrite(i, HIGH);
+      delay(100);
+    }
+
+    for(int i=13; i > 8; i--){
+      digitalWrite(i, LOW);
+      delay(100);
+    }
+  }
+
+}

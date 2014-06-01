5d487f3da5aa07e4240bfd027f267bbaa2414fe2
Paul Wilson <paulanthonywilson@gmail.com>
2014-06-01 16:36:45 +0100

    attempt to send

--- radiobathroom/transmit/transmit.ino
+++ radiobathroom/transmit/transmit.ino
@@ -1,64 +1,72 @@
 #include <avr/sleep.h>
+#include <VirtualWire.h>
 
 
 int wakePin = 2;                 // pin used for waking up
 int sleepStatus = 0;             // variable to store a request for sleep
 int count = 0;                   // counter
 
-volatile unsigned long wakeCount = 0;
-void wakeUpNow() {
-  wakeCount++;
-}
+void wakeUpNow() { }
+
+void (*loopState)();
+unsigned long millisEnteredState = 0l;
 
 void setup() {
   pinMode(wakePin, INPUT_PULLUP);
+  vw_setup(2000); // Bits per sec
   Serial.begin(9600);
-  attachInterrupt(0, wakeUpNow, LOW); 
+}
+
+void send (char *message) {
+  vw_send((uint8_t *)message, strlen(message));
+  vw_wait_tx(); // Wait until the whole message is gone
+  Serial.println(message);
 }
 
 void sleepNow() {
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    sleep_enable();
-    attachInterrupt(0,wakeUpNow, LOW); 
-    sleep_mode(); // blocks
-    
-    sleep_disable();
-    detachInterrupt(0);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  attachInterrupt(0,wakeUpNow, LOW);
+  sleep_mode(); // blocks
+
+  sleep_disable();
+  detachInterrupt(0);
 }
 
-void loop()
-{
-  // display information about the counter
-  Serial.print("Awake for ");
-  Serial.print(count);
-  Serial.print("sec");
-  Serial.print(".  Wake count: ");
-  Serial.println(wakeCount);
-  count++;
-  delay(1000);                           // waits for a second
+bool isDoorClosed(){
+  return digitalRead(2) == LOW;
+}
 
-  // compute the serial input
-  if (Serial.available()) {
-    int val = Serial.read();
-    if (val == 'S') {
-      Serial.println("Serial: Entering Sleep mode");
-      delay(100);     // this delay is needed, the sleep 
-                      //function will provoke a Serial error otherwise!!
-      count = 0;
-      sleepNow();     // sleep function called here
-    }
-    if (val == 'A') {
-      Serial.println("Hola Caracola"); // classic dummy message
-    }
-  }
+long millisInState(){
+  return millis() - millisEnteredState;
+}
 
-  // check if it should go to sleep because of time
-  if (count >= 10) {
-      Serial.println("Timer: Entering Sleep mode");
-      delay(100);     // this delay is needed, the sleep 
-                      //function will provoke a Serial error otherwise!!
-      count = 0;
-      sleepNow();     // sleep function called here
+void setLoopState(void (*newState)()){
+  if(loopState != newState){
+    millisEnteredState = millis();
+    loopState = newState;
   }
+}
+
+void doorOpenLoop(){
+  Serial.print("open ");
+  send("The bathroom door is open. Thank you.");
+}
+void doorClosedLoop(){
+  Serial.print("closed ");
+  send("The bathroom door is closed. Thank you.");
+}
+
+
+void loop() {
+  if(isDoorClosed()){
+    setLoopState(doorOpenLoop);
+  } else {
+    setLoopState(doorClosedLoop);
+  }
+  (*loopState)();
+  Serial.print(millisInState());
+  Serial.print(" ");
+  Serial.println(isDoorClosed());
 }
 
